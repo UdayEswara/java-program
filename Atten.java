75011719e3918be8b220f90163c2fba57287ffe6
UdayeswaraReddy <136412972+UdayEswara@users.noreply.github.com>
2023-10-11 18:02:08 +0530

    Update Atten.java

--- Atten.java
+++ Atten.java
@@ -3,7 +3,7 @@ public class Atten
 {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-	    for(int i=1;i<=1;i++)
+	    for(int i=1;i<=10;i++)
 	    {
 	        System.out.println("Enter the name:");
 	        String name = sc.next();
